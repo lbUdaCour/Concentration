a815073ecebecbd848a3d295aeda4365b2144dbc
L BOWEN <ljbowen@l-bowens-mbp.attlocal.net>
2019-03-05 14:56:45 -0600

    One button has been added with the code to flip

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        //print("agh! a ghost!")
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.6462134719, blue: 0.1945535839, alpha: 1)
+        } else {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
     }
 
-
 }
-
